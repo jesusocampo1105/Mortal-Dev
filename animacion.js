e9d50411c4bf37aa35d431139c46ac1081bf15f4
Óscar O <oscarforjuela@hotmail.com>
2022-11-29 18:11:56 -0500

    funcionalidad

--- animacion.js
+++ animacion.js
@@ -1,1 +1,16 @@
-//document.getElementById("pelea").style.backgroundColor = "red";
+var personaje = document.getElementById("poder");
+var boton = document.getElementById("pausa");
+
+if(personaje.classList && personaje && boton){
+    boton.onclick = function(){
+        if(this.value == 'pause'){
+            personaje.classList.add('pause');
+            this.value = 'play';
+        }else{
+            personaje.classList.remove('pause');
+            this.value = 'pause';
+        }
+    }
+}
+
+
