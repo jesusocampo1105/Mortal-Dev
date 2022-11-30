3dfacd50c6114af1290c21994d04fd86d6d7a101
Óscar O <oscarforjuela@hotmail.com>
2022-11-30 12:39:23 -0500

    Función lanzar

--- animacion.js
+++ animacion.js
@@ -1,16 +1,16 @@
 var personaje = document.getElementById("poder");
 var boton = document.getElementById("pausa");
 
-if(personaje.classList && personaje && boton){
-    boton.onclick = function(){
-        if(this.value == 'Kill'){
-            personaje.classList.add('pause');
-            this.value = 'Finish Him';
-        }else{
-            personaje.classList.remove('pause');
-            this.value = 'Kill';
-        }
+function lanzar(){
+    if(this.value == 'Kill'){
+        personaje.classList.add('pause');
+        this.value = 'Finish Him';
+    }else{
+        personaje.classList.remove('pause');
+        this.value = 'Kill';
     }
 }
 
-
+if(personaje.classList && personaje && boton){
+    boton.addEventListener("click", lanzar);  
+}
