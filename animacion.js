85b29eb0b13126c68355371987ca5d92fed66750
Óscar O <oscarforjuela@hotmail.com>
2022-12-01 10:53:22 -0500

    Animación

--- animacion.js
+++ animacion.js
@@ -1,13 +1,17 @@
 var personaje = document.getElementById("poder");
 var boton = document.getElementById("pausa");
 var punos = document.getElementById("pelea");
+var detenidog = document.getElementById("detenido");
 
-
+function detener(){
+    detenidog.style.display = 'none';
+}
 
 function lanzar(){
     if(this.value == 'Kill'){
         personaje.classList.add('pause');
         this.value = 'Finish Him';
+        detener();
         //personaje.style.display = 'none';
     }else{
         personaje.classList.remove('pause');
