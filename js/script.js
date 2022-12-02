f1561ece6b782ede0f27ba01360a4fe0db447a8d
DiegoRodriguez1203 <diegorodriguez1203@gmail.com>
2022-12-02 12:57:34 -0500

    git Merge branch 'main' of https://github.com/jesusocampo1105/Mortal-Dev

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let players = [];
+//  Add player
+let nombre = document.getElementById('nombre');
+//  Player list
+let nombres = document.getElementById('nombres');
+// Add boton
+let add = document.getElementById('add');
+
